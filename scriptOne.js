b96f1eef632438f7b0b5d7a4f0002a1ab49cfe3f
Nirma Abro <NirmaAbro143@gmail.com>
2024-06-29 23:32:30 -0600

    cookies & sessions , form handling

--- scriptOne.js
+++ scriptOne.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+//firstly i installed cookie-parser then write it these two lines
+
+// Purpose: This middleware is used to parse incoming requests with JSON payloads. It helps your application understand and process JSON data sent in the body of HTTP requests.
+// Usage: It is essential for handling API requests where the client (like a React app) sends data in JSON format (e.g., POST requests with JSON bodies).
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Middleware to log requests,//jab bhi server request acceptkrta ha vaha sa route k bech phnchny tak agr ap us request ko bech ma rokty  ho , r kuch perform krty ho to ya element middleware kehlata ha
 app.use(function (req, res, next) {
   console.log("middleware");
